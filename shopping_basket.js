a91475d80399abc1634405faa15edcaa1345469c
User <user@Users-MacBook-Pro-7.local>
2016-01-26 15:36:13 +0000

    10% discount working

--- shopping_basket.js
+++ shopping_basket.js
@@ -6,13 +6,22 @@ var shopping_basket = {
   add_item: function(item){
     this.items.push(item);
   },
-  items_price:function(items){
+  items_price:function(){
     var totalPrice = 0
     for(item of this.items){
      totalPrice += item.price;
     }
     return totalPrice
   },
+
+  discount_price:function(){
+    if(this.items_price() >= 20){
+      discountPrice = (this.items_price() * 0.9);
+    }else{
+      discountPrice = this.items_price();
+    }
+    return discountPrice;
+  }
 
 }
 
--- shopping_basket_spec.js
+++ shopping_basket_spec.js
@@ -3,7 +3,7 @@ var assert = require('assert');
 
 var beans = {
   name: "baked beans",
-  price: 1,
+  price: 20,
   bogof: true
 }
 
@@ -12,8 +12,13 @@ describe('Shopping Basket', function(){
   it('should be empty array at start', function(){
     assert.equal(undefined, shopping_basket.items[0]);
   });
-  it('first item name should be baked beans', function(){
+  it('first item name should be baked beans and price should be 1', function(){
     shopping_basket.add_item(beans)
     assert.equal("baked beans", shopping_basket.items[0].name);
+    assert.equal(20, shopping_basket.items_price());
   });
+  it('discounted price should be 1', function(){
+    assert.equal(18, shopping_basket.discount_price());
+  });
+  
 });
